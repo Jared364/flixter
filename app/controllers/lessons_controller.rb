888a1a7f0fc1ffbbebe2598d2936b2cd8975c568
Jared364 <jaredand1994@gmail.com>
2019-11-21 02:11:45 +0000

    updated lessons controller to check if user is enrolled, and redirect otherwise.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,18 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
+  before_action :is_user_enrolled?
 
 
   def show
   end
 
   private
+
+  def is_user_enrolled?
+    unless current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_url(current_lesson.section.course), alert: 'Please Enroll to Continue'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
