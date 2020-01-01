a948c43c3e5f849db6d12a481eed1b10d2966e7b
Jared364 <jaredand1994@gmail.com>
2020-01-01 17:10:46 +0000

    Added Privacy/ Careers/ About Page routes to be edited for later.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,13 @@
 class StaticPagesController < ApplicationController
   def index
+  end
 
+  def privacy
+  end
+
+  def about
+  end
+
+  def career
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resource :dashboard, only: [:show]
   root 'static_pages#index'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'about', to: 'static_pages#about'
+  get 'career', to: 'static_pages#career'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
